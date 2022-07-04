8603dfd2c9370568874c7afd3ef3d81d0cfdb96c
Neilwinn Pineda <chaosg0hd@gmail.com>
2022-07-04 19:58:30 +0800

    GameMenuSciprt

--- Scripts/MenuController.cs
+++ Scripts/MenuController.cs
@@ -2,17 +2,118 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuScript : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private string MenuState = "none";
+
+
+    [SerializeField]
+    private GameObject MainPanel;
+
+    [SerializeField]
+    private GameObject OptionPanel;
+
+    [SerializeField]
+    private GameObject CreditsPanel;
+
+    [SerializeField]
+    private GameObject ExitPanel;
+
+    public void setState(string state)
     {
-        
+        this.MenuState = state;
+
+        switch (state)
+        {
+            case "none":
+                MainPanel.SetActive(true);
+
+                break;
+            case "start":
+                //code for scene change
+
+                break;
+
+            case "settings":
+                MainPanel.SetActive(false);
+                OptionPanel.SetActive(true);
+                break;
+
+            case "credits":
+                MainPanel.SetActive(false);
+                CreditsPanel.SetActive(true);
+                break;
+
+            case "exit":
+                ExitPanel.SetActive(true);
+                break;
+
+
+            case "back":
+                MainPanel.SetActive(true);
+                break;
+
+
+            default:
+                break;
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+    //public void setStateStart()
+    //{
+
+    //    //code for scene change
+    //    this.MenuState = "start";
+    //}
+
+    //public void setStateOptions()
+    //{   
+
+    //    this.MenuState = "options";
+    //    this.OptionsPanel = 
+    //}
+
+    //public void setStateMain()
+    //{
+    //    this.MenuState = "main";
+    //}
+
+    //public void setStateStart()
+    //{
+    //    this.MenuState = state;
+    //}
+
+    //public void setStateStart()
+    //{
+    //    this.MenuState = state;
+    //}
+
+    //public void setStateStart()
+    //{
+    //    this.MenuState = state;
+    //}
+
+    //private void MenuManager()
+    //{
+
+    //}
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
 }
