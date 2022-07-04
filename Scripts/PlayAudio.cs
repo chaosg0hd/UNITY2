6ea01f252a39b8481144fba8d2b0d45513b4e4b8
Mark Jerico Fabro <201811325@gordoncolle.edu.ph>
2022-07-04 18:38:27 +0800

    Main Menu Frontend
    UI Progression 50%

---
+++ Scripts/PlayAudio.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public GameObject audioObj;
+
+    public void DropAudio() {
+        Instantiate(audioObj, transform.position, transform.rotation);
+    }
+}
